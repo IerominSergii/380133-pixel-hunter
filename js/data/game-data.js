ea34d1d82ca9231375ba53db029b1c52cb710e5c
IerominSergii <iconboooook@gmail.com>
2018-04-08 22:20:39 +0300

    Добавь функцию создания таймера

--- js/data/game-data.js
+++ js/data/game-data.js
@@ -23,11 +23,29 @@ const LIFE_VALUE = 50;
 
 // посчитать заработанные набранные очки
 const countSumPoints = (gameObject) => {
-  const answersArr = gameObject.answers;
+  const answers = gameObject.answers;
+  if (typeof answers !== `object`) {
+    return -1;
+    // throw new Error(`Wrong type of answers. Expected array`);
+  }
 
-  const fastAnswers = answersArr.filter((item) => item === answerType.fast);
-  const slowAnswers = answersArr.filter((item) => item === answerType.slow);
-  const rightAnswers = answersArr.filter((item) => item === answerType.right);
+  if (answers.length > 0 && answers.length < 10) {
+    return -1;
+  }
+
+  if (gameObject.life < 0 || gameObject.life > 3) {
+    return -1;
+    // throw new Error(`Wrong life amount. Expected from 0 to 3`);
+  }
+
+  const wrongAnswers = answers.filter((item) => item === answerType.wrong);
+  if (wrongAnswers > 3) {
+    return -1;
+  }
+
+  const fastAnswers = answers.filter((item) => item === answerType.fast);
+  const slowAnswers = answers.filter((item) => item === answerType.slow);
+  const rightAnswers = answers.filter((item) => item === answerType.right);
 
   const sumPoints = (fastAnswers.length * ANSWER_VALUE.fast + slowAnswers.length * ANSWER_VALUE.slow + rightAnswers.length * ANSWER_VALUE.right);
 
@@ -36,28 +54,42 @@ const countSumPoints = (gameObject) => {
 
 // посчитать очки за оставш. жизни
 const countSumLifes = (gameObject) => {
-  return gameObject.life * LIFE_VALUE;
-};
-
-// подсчет результатов игры
-const checkGameResult = (gameObject) => {
-  const answers = gameObject.answers;
-  if (typeof answers !== `object`) {
-    throw new Error(`Wrong type of answers. Expected array`);
-  }
-
-  if (answers.length > 0 && answers.length < 10) {
+  if (gameObject.life < 0 || gameObject.life > 3) {
+    // throw new Error(`Wrong lifes amount. Expect from 0 to 3.`);
     return -1;
   }
 
-  if (gameObject.life < 0 || gameObject.life > 3) {
-    throw new Error(`Wrong life amount. Expected from 0 to 3`);
-  }
+  return gameObject.life * LIFE_VALUE;
+};
+
+// подсчет итоговых результатов игры
+const checkGameResult = (gameObject) => {
 
   const pointsResult = countSumPoints(gameObject);
   const lifesResult = countSumLifes(gameObject);
 
+  if (pointsResult === -1 || lifesResult === -1) {
+    return -1;
+  }
   return pointsResult + lifesResult;
 };
 
-export {MAX_QUESTION_AMOUNT, gameData, checkGameResult};
+// функция создания таймера
+const createTimer = (time) => {
+  const timerObj = {timer: time};
+  return timerObj;
+};
+
+const tick = (timerObj) => {
+  if (timerObj.timer < 0 || timerObj.timer > 30) {
+    throw new Error(`Wrong time. Expect from 0 to 30`);
+  }
+
+  if (timerObj.timer === 0) {
+    return -1;
+  }
+
+  return timerObj.timer - 1;
+};
+
+export {MAX_QUESTION_AMOUNT, gameData, checkGameResult, createTimer, tick};
--- js/data/game-data.test.js
+++ js/data/game-data.test.js
@@ -14,17 +14,18 @@ describe(`Check game results`, () => {
       assert.equal(checkGameResult(testData4), -1);
     });
 
-    // it(`should not allow wrong type`, () => {
-    //   const testNumberData = {answers: 103};
-    //   assert.equal(checkGameResult(testNumberData), -1);
-    //   const testStringData = {answers: `testString`};
-    //   assert.equal(checkGameResult(testStringData), -1);
-    //   const testBooleanData = {answers: true};
-    //   assert.equal(checkGameResult(testBooleanData), -1);
-    //   let testUndefined;
-    //   const testUndefinedData = {answers: testUndefined};
-    //   assert.equal(checkGameResult(testUndefinedData), -1);
-    // });
+    // разберись, как тестировать ошибки
+    it(`should not allow wrong type`, () => {
+      const testNumberData = {answers: 103};
+      assert.equal(checkGameResult(testNumberData), -1);
+      const testStringData = {answers: `testString`};
+      assert.equal(checkGameResult(testStringData), -1);
+      const testBooleanData = {answers: true};
+      assert.equal(checkGameResult(testBooleanData), -1);
+      let testUndefined;
+      const testUndefinedData = {answers: testUndefined};
+      assert.equal(checkGameResult(testUndefinedData), -1);
+    });
 
     it(`should count according answerValue (answer time)`, () => {
       const testData1 = {
@@ -59,10 +60,25 @@ describe(`Check game results`, () => {
     });
   });
 
-  // describe(`Check game lifes`, () => {
-  //   it(`should count lifes properly`, () => {
-  //     const testData1 = {answers: [10, 2, 3, 4, 5, 5, `string`, 7, 8]};
-  //     assert.equal(checkGameResult(testData1), -1);
-  //   });
-  // });
+  describe(`Check game lifes`, () => {
+    it(`should count lifes properly`, () => {
+      const testData1 = {
+        answers: [`right`, `right`, `right`, `right`, `right`, `right`, `right`, `right`, `right`, `right`],
+        life: -2,
+      };
+      assert.equal(checkGameResult(testData1), -1);
+
+      const testData2 = {
+        answers: [`right`, `right`, `right`, `right`, `right`, `right`, `right`, `right`, `right`, `right`],
+        life: 4,
+      };
+      assert.equal(checkGameResult(testData2), -1);
+
+      const testData3 = {
+        answers: [`right`, `right`, `right`, `right`, `right`, `right`, `right`, `right`, `right`, `right`],
+        life: 5,
+      };
+      assert.equal(checkGameResult(testData3), -1, `Expected -1 but recieved ${testData3.life}`);
+    });
+  });
 });
