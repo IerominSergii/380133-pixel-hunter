c065fa78c589a30464174d578b2d4e29fdad15aa
IerominSergii <iconboooook@gmail.com>
2018-05-14 11:38:44 +0300

    Перенеси логику переключения экранов и создания вьюшек в game-screen

--- js/game/game-screen.js
+++ js/game/game-screen.js
@@ -1,14 +1,57 @@
+import IntroView from './intro-view';
+import GreetingView from './greeting-view';
+import RulesView from './rules-view';
+import HeaderView from './header-view';
+import FooterView from './footer-view';
+
+
 import LevelView from './level-view';
 import StatsView from './stats-view';
-import HeaderView from './header-view';
-import GreetingView from './greeting-view';
-import FooterView from './footer-view';
+
 
 import {INITIAL_GAME, changeLevel, addResult, canContinue, die} from '../data/game-data';
 import {createCustomElement, updateView, changeView} from './../util';
 import {questions} from '../data/questions-data';
 import {resultStatus} from '../constant';
 
+
+// создаю вьюшки
+const intro = new IntroView();
+const greeting = new GreetingView();
+const rules = new RulesView();
+const shortHeader = new HeaderView();
+const footer = new FooterView();
+
+const gameContainer = document.querySelector(`.central`);
+
+// ===== переопределяю функции-исполнители во вьюшках =====
+rules.startGame = () => {
+  rules.removeRules();
+  gameContainer.insertBefore(gameFragment, gameContainer.firstChild);
+};
+
+shortHeader.onBackArrowClick = () => {
+  changeView(greeting.element);
+  gameContainer.appendChild(footer.element);
+};
+
+intro.onAsteriskClick = () => {
+  changeView(greeting.element);
+  gameContainer.appendChild(footer.element);
+};
+
+greeting.onContinueClick = () => {
+  changeView(rules.element);
+
+  // автофокус
+  const input = document.querySelector(`.rules__input`);
+  input.focus();
+
+  gameContainer.prepend(shortHeader.element);
+  gameContainer.append(footer.element);
+};
+
+// ===== game logic ======
 let gameState = Object.assign({}, INITIAL_GAME);
 
 const gameFragment = document.createDocumentFragment();
@@ -17,10 +60,6 @@ const levelContainer = createCustomElement(``, `div`, `game`);
 
 gameFragment.appendChild(headerContainer);
 gameFragment.appendChild(levelContainer);
-
-const greeting = new GreetingView();
-const footer = new FooterView();
-const gameContainer = document.querySelector(`.central`);
 
 const continueOrDie = () => {
   gameState = changeLevel(gameState, gameState.level + 1);
@@ -71,14 +110,14 @@ const onTripleUserAnswer = (answer) => {
 };
 
 const updateGame = (state) => {
-  const header = new HeaderView(state.life, state.timer);
+  const fullHeader = new HeaderView(state.life, state.timer);
 
-  header.onBackArrowClick = () => {
+  fullHeader.onBackArrowClick = () => {
     changeView(greeting.element);
     gameContainer.appendChild(footer.element);
   };
 
-  updateView(headerContainer, header);
+  updateView(headerContainer, fullHeader);
 
   let levelNumber = state.level;
   const currentQuestion = questions[levelNumber];
@@ -107,4 +146,5 @@ const playGame = () => {
 
 playGame();
 
-export default gameFragment;
+// export default gameFragment;
+export {intro};
--- js/main.js
+++ js/main.js
@@ -1,45 +1,4 @@
-import IntroView from './game/intro-view';
-import GreetingView from './game/greeting-view';
-import RulesView from './game/rules-view';
-import HeaderView from './game/header-view';
-import FooterView from './game/footer-view';
-import gameFragment from './game/game-screen';
-
 import {changeView} from './util';
-
-
-const intro = new IntroView();
-export const greeting = new GreetingView();
-const rules = new RulesView();
-const header = new HeaderView();
-const footer = new FooterView();
-
-const gameContainer = document.querySelector(`.central`);
-
-rules.startGame = () => {
-  rules.removeRules();
-  gameContainer.insertBefore(gameFragment, gameContainer.firstChild);
-};
-
-header.onBackArrowClick = () => {
-  changeView(greeting.element);
-  gameContainer.appendChild(footer.element);
-};
-
-intro.onAsteriskClick = () => {
-  changeView(greeting.element);
-  gameContainer.appendChild(footer.element);
-};
-
-greeting.onContinueClick = () => {
-  changeView(rules.element);
-
-  // автофокус
-  const input = document.querySelector(`.rules__input`);
-  input.focus();
-
-  gameContainer.prepend(header.element);
-  gameContainer.append(footer.element);
-};
+import {intro} from './game/game-screen';
 
 changeView(intro.element);
